0db10618d884f2894409c523e89b325baed602f7
flvtisv <flatislove@gmail.com>
2023-02-23 19:56:24 +0600

    init

---
+++ ta_training-java/src/main/java/com/epam/training/student_uladzislau_valynets/App.java
@@ -0,0 +1,12 @@
+package com.epam.training.student_uladzislau_valynets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println();
+    }
+}
